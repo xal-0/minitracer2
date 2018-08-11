2374f4862eb47281dc150f8be3731ff68c4ea00b
Sam Schweigel <s.schweigel@gmail.com>
2018-08-11 00:31:50 -0700

    Basic linenumber mappings done!

--- src/debug/dwarf.cpp
+++ src/debug/dwarf.cpp
@@ -4,9 +4,11 @@ using namespace std;
 
 namespace dwarf {
 
-dwarf::dwarf(std::istream &stream)
+dwarf::dwarf(istream &stream)
     : stream(stream), elf(stream)
 {}
+
+size_t soffset;
 
 void dwarf::read_linenums()
 {
@@ -19,33 +21,149 @@ void dwarf::read_linenums()
 
     stream.seekg(sec.offset);
 
-    linenum_prog p {stream};
+    soffset = sec.offset;
+
+    while (stream.tellg() < static_cast<long>(sec.size + sec.offset)) 
+        linenum_prog p {*this};
+
+    for (const auto &x : line_mappings)
+        cout << hex << x.address << "\t\t\t" << x.file->filename << ":" << dec << x.line << "\n";
 }
 
-dwarf::linenum_prog::linenum_prog(istream &stream)
+bool operator<(const dwarf::meta_file &a, const dwarf::meta_file &b)
 {
-    header = read_obj<linenum_header>(stream);
+    return a.filename < b.filename;
+}
+
+dwarf::linenum_prog::linenum_prog(dwarf &d)
+{
+    u32 start = d.stream.tellg();
+    header = read_obj<linenum_header>(d.stream);
 
     if (header.unit_length == 0xffffffff)
         throw invalid_argument("only 32-bit DWARF is supported");
 
-    if (header.version != 2)
+    if (header.version > 2)
         throw invalid_argument("only DWARF2 is supported");
 
     standard_opcode_lengths.reserve(header.opcode_base - 1);
     for (int i = 0; i < header.opcode_base - 1; i++)
-        standard_opcode_lengths[i] = read_obj<u8>(stream);
+        standard_opcode_lengths.push_back(read_obj<u8>(d.stream));
 
+    string dir;
+    while (!(dir = read_str(d.stream)).empty()) {
+        const string *ref = &*d.include_directories.insert(dir).first;
+        include_directories.push_back(ref);
+    }
 
-    std::string dir;
-    while (!(dir = read_str(stream)).empty())
-        include_directories.push_back(dir);
+    string filename;
+    while (!(filename = read_str(d.stream)).empty()) {
+        meta_file file;
+        file.filename = filename;
+        file.directory = include_directories[read_leb(d.stream)];
+        file.last_modified = read_leb(d.stream);
+        file.len = read_leb(d.stream);
+        const meta_file *ref = &*d.file_names.insert(file).first;
+        file_names.push_back(ref);
+    }
 
-    std::string file;
-    while (!(file = read_str(stream)).empty())
-        file_names.push_back(file);
+    is_stmt = header.default_is_stmt;
 
-    
+    while (d.stream.tellg() < header.unit_length + start + 4) 
+        execute(d);
+}
+
+void dwarf::linenum_prog::execute(dwarf &d)
+{
+    u8 op = read_obj<u8>(d.stream);
+
+    if (op == 0)
+        execute_extended(d);
+    else if (op < header.opcode_base)
+        execute_standard(d, op);
+    else {
+        line += header.line_base +
+            ((op - header.opcode_base) % header.line_range);
+        address += ((op - header.opcode_base) / header.line_range)
+            * header.minimum_instruction_length;
+        basic_block = false;
+
+        copy_matrix(d);
+    }
+}
+
+void dwarf::linenum_prog::execute_extended(dwarf &d)
+{
+    u32 len = read_leb(d.stream);
+    u8 op = read_obj<u8>(d.stream);
+
+    switch (op) {
+    case 1:
+        end_sequence = true;
+        copy_matrix(d);
+
+        address = 0;
+        file = 1;
+        line = 1;
+        column = 0;
+        is_stmt = header.default_is_stmt;
+        basic_block = false;
+        end_sequence = false;
+
+        break;
+
+    case 2:
+        address = read_obj<uaddr>(d.stream);
+        break;
+
+    default:
+        d.stream.seekg(len, ios_base::cur);
+    }
+}
+
+void dwarf::linenum_prog::execute_standard(dwarf &d, u8 op)
+{
+    switch (op) {
+    case 1:
+        copy_matrix(d);
+        basic_block = false;
+        break;
+
+    case 2:
+        address += read_leb(d.stream) * header.minimum_instruction_length;
+        break;
+
+    case 3:
+        line += read_leb(d.stream);
+        break;
+
+    case 4:
+        file = read_leb(d.stream);
+        break;
+
+    case 5:
+        column = read_leb(d.stream);
+        break;
+
+    default:
+        for (int i = 0; i < standard_opcode_lengths[op - 1]; i++)
+            read_leb(d.stream);
+    }
+}
+
+bool operator<(dwarf::line_map const &a, dwarf::line_map const &b)
+{
+    return a.address < b.address;
+}
+
+void dwarf::linenum_prog::copy_matrix(dwarf &d)
+{
+    line_map m;
+    m.address = address;
+    m.line = line;
+    m.file = file_names[file - 1];
+
+    d.line_mappings.insert(m);
 }
 
 };
--- src/debug/dwarf.hpp
+++ src/debug/dwarf.hpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <set>
 
 #include "elf.hpp"
 #include "arch/arch.hpp"
@@ -16,10 +17,27 @@ public:
     
     void read_linenums();
 
+    struct meta_file {
+        std::string filename;
+        const std::string *directory;
+        u32 last_modified;
+        u32 len;
+
+        friend bool operator<(meta_file const &a, meta_file const &b);
+    };
+
+    struct line_map {
+        uaddr address;
+        const meta_file *file;
+        u32 line;
+
+        friend bool operator<(line_map const &a, line_map const &b);
+    };
+
 protected:
     class linenum_prog {
     public:
-        linenum_prog(std::istream &stream);
+        linenum_prog(dwarf &d);
 
     protected:
         STARTPACK
@@ -27,7 +45,7 @@ protected:
             u32 unit_length;
             u16 version;
             u32 header_length;
-            u8 minimum_intstruction_length;
+            u8 minimum_instruction_length;
             u8 default_is_stmt;
             i8 line_base;
             u8 line_range;
@@ -35,16 +53,35 @@ protected:
         };
         ENDPACK
 
+        void execute(dwarf &d);
+        void execute_extended(dwarf &d);
+        void execute_standard(dwarf &d, u8 op);
+
+        void copy_matrix(dwarf &d);
+
     private:
         linenum_header header;
         std::vector<u8> standard_opcode_lengths;
-        std::vector<std::string> include_directories;
-        std::vector<std::string> file_names;
+        std::vector<const std::string *> include_directories;
+        std::vector<const meta_file *> file_names;
+
+        uaddr address = 0;
+        u32 file = 1;
+        u32 line = 1;
+        u32 column = 0;
+        bool is_stmt;
+        bool basic_block = false;
+        bool end_sequence = false;
     };
 
 private:
     std::istream &stream;
     elf::elf elf;
+
+    std::set<std::string> include_directories;
+    std::set<meta_file> file_names;
+
+    std::set<line_map> line_mappings;
 };
 
 };
